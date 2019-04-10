05e01a5e65095606069e7b1b35960abef14add80
jonathan <jonlaor86@gmail.com>
2019-04-10 17:15:22 +0300

    first commit

---
+++ bootcamp/week1/main.js
@@ -0,0 +1,10 @@
+const bag = {
+    weight: 10,
+    owner: "Larissa",
+    items: [                   // an array with objects in it!
+      {item: "Pen", count: 2},
+      {item: "Bottle", count: 1},
+      {item: "Book", count: 3}
+    ]
+  }
+  console.log(bag.items)
